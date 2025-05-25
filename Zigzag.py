069776ac5c416c773fd1efdcfa3ccf18826429a1
Banuraj123 <kadechurbanuraj@gmail.com>
2025-05-25 19:22:33 +0530

    Add files via upload

---
+++ Zigzag.py
@@ -0,0 +1,13 @@
+print('NAME:BANURAJ B K' ,'USN:1AY24AI015') 
+def run_zigzag(num_asterisks): 
+    indentation=0 
+    direction=1 # 1 for increasing,-1 for decreasimg 
+ 
+    while num_asterisks > 0: 
+        print(" "*indentation + "*") 
+        indentation+=direction 
+        num_asterisks-=1 
+ 
+        if indentation==0  or indentation==5:#adjust upper limit as needed 
+            direction*=-1 
+run_zigzag(50) 
