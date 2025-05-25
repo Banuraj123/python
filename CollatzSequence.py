518d08d79217c3d4b9204180e32da8084a781f36
Banuraj123 <kadechurbanuraj@gmail.com>
2025-05-25 19:18:53 +0530

    Add files via upload

---
+++ CollatzSequence.py
@@ -0,0 +1,10 @@
+print('NAME:BANURAJ B K' ,'USN:1AY24AI015' \
+'')
+n=int(input("enter a number:"))
+while n!=1:
+    print(n,end='->')
+    if(n%2==0):
+        n=n//2
+    else:
+        n=3*n+1
+print(1)
